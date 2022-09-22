3a97199ea879061dcb7b1df7b1b0d11e3d84f0c5
Maycon Jesus <email@mayconjesus.dev>
2022-09-22 19:17:30 -0300

    feat: change password

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -13,6 +13,11 @@ export class AuthController extends Controller {
         password: joi.string().required()
     }).required()
 
+    private _changePasswordDataValidator = joi.object({
+        oldPassword: joi.string().required().max(255),
+        newPassword: joi.string().required().max(255)
+    }).required()
+
     async login (data: Object):Promise<PromiseResolveData<{
         token: string
     }>> {
@@ -61,6 +66,51 @@ export class AuthController extends Controller {
             code: 200,
             data: {
                 token
+            }
+        }
+    }
+
+    async changePassword (userId:number, data: Object) :Promise<PromiseResolveData<{
+        success:boolean
+    }>> {
+        const dataValid = this._changePasswordDataValidator.validate(data)
+        if (dataValid.error?.message) {
+            return {
+                code: 400,
+                message: dataValid.error.message
+            }
+        }
+
+        const user = await this.dd.database.db.user.findFirst({
+            where: { id: userId },
+            select: { password: true }
+        })
+        if (!user) {
+            return {
+                code: 404,
+                message: 'Usuário não encontrado'
+            }
+        }
+
+        const oldPasswordCorrect = bcrypt.compareSync(dataValid.value.oldPassword, user.password)
+        if (!oldPasswordCorrect) {
+            return {
+                code: 403,
+                message: 'A sua senha atual esta incorreta!'
+            }
+        }
+
+        await this.dd.database.db.user.update({
+            data: {
+                password: bcrypt.hashSync(dataValid.value.newPassword, 14)
+            },
+            where: { id: userId }
+        })
+
+        return {
+            code: 200,
+            data: {
+                success: true
             }
         }
     }
